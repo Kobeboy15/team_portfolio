9385c100519cee1e69481d68477315d6ee2de23b
stahsin00 <lamisa1998@hotmail.com>
2026-03-16 11:50:18 -0700

    feat: create about timeline data

---
+++ src/data/timeline.ts
@@ -0,0 +1,43 @@
+import type { TimelineYear } from "../types/about";
+
+export const timelineData: TimelineYear[] = [    // Placeholders for now, will be replaced with actual data later
+  {
+    year: "2023",
+    entries: [
+      {
+        image: "/images/timeline/timeline-2023-01.webp",
+        imageAlt: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+      },
+      {
+        image: "/images/timeline/timeline-2023-02.webp",
+        imageAlt: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+      },
+      {
+        image: "/images/timeline/timeline-2023-03.webp",
+        imageAlt: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+      }
+    ]
+  },
+  {
+    year: "2024",
+    entries: [
+      {
+        image: "/images/timeline/timeline-2024-01.webp",
+        imageAlt: "",
+      },
+      {
+        image: "/images/timeline/timeline-2024-02.webp",
+        imageAlt: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+      },
+      {
+        image: "/images/timeline/timeline-2024-03.webp",
+        imageAlt: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+      }
+    ]
+  }
+];
