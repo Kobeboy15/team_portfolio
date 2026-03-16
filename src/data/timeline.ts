126370e1883f937051678fb06718eba588da977e
stahsin00 <lamisa1998@hotmail.com>
2026-03-16 12:15:53 -0700

    fix: address coderabbit found issues

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -5,18 +5,18 @@ export const timelineData: TimelineYear[] = [    // Placeholders for now, will b
     year: "2023",
     entries: [
       {
-        image: "/images/timeline/timeline-2023-01.webp",
-        imageAlt: "",
+        image: "/images/about/timeline/timeline-2023-01.webp",
+        imageAlt: "Timeline event placeholder",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
       },
       {
-        image: "/images/timeline/timeline-2023-02.webp",
-        imageAlt: "",
+        image: "/images/about/timeline/timeline-2023-02.webp",
+        imageAlt: "Timeline event placeholder",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
       },
       {
-        image: "/images/timeline/timeline-2023-03.webp",
-        imageAlt: "",
+        image: "/images/about/timeline/timeline-2023-03.webp",
+        imageAlt: "Timeline event placeholder",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
       }
     ]
@@ -25,18 +25,18 @@ export const timelineData: TimelineYear[] = [    // Placeholders for now, will b
     year: "2024",
     entries: [
       {
-        image: "/images/timeline/timeline-2024-01.webp",
-        imageAlt: "",
+        image: "/images/about/timeline/timeline-2024-01.webp",
+        imageAlt: "Timeline event placeholder",
       },
       {
-        image: "/images/timeline/timeline-2024-02.webp",
-        imageAlt: "",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+        image: "/images/about/timeline/timeline-2024-02.webp",
+        imageAlt: "Timeline event placeholder",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2024)"
       },
       {
-        image: "/images/timeline/timeline-2024-03.webp",
-        imageAlt: "",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2023)"
+        image: "/images/about/timeline/timeline-2024-03.webp",
+        imageAlt: "Timeline event placeholder",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit imperdiet quam posuere (3/1/2024)"
       }
     ]
   }
