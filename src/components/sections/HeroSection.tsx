c16f1ba65a7bb59a94f0953a5cc64a9c10248b87
EnriqueSaracho <esarachof97@gmail.com>
2026-03-18 18:19:01 -0700

    feat: build remaining screen sizes hero section designs

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -14,7 +14,7 @@ export function HeroSection() {
         </div>
 
         <div className="flex flex-col sm:flex-row sm:flex-1">
-          <div className="flex flex-col items-end sm:items-start justify-start px-3 pt-20 sm:pl-3 md:pl-9 sm:pr-2 md:pr-5 gap-y-2 sm:gap-y-4">
+          <div className="flex flex-col items-end sm:items-start justify-start px-3 pt-20 sm:pl-3 md:pl-4 lg:pl-9 sm:pr-2 md:pr-3 lg:pr-5 xl:pr-7 gap-y-2 sm:gap-y-4">
             <p className="text-sora-18 ">
               I&apos;m a software engineer with a passion for building web applications that are not only functional but also beautiful and easy to
               use.
@@ -37,12 +37,12 @@ export function HeroSection() {
             </Button>
           </div>
 
-          <div className="relative w-full sm:w-[380px] sm:h-[450px] md:w-[515px] md:h-[386px] xl:w-[773px] xl:h-[580px] xl:shrink-0 aspect-[773/580] xl:aspect-auto px-3 sm:px-0">
+          <div className="relative w-full sm:w-[380px] sm:h-[450px] md:w-[420px] md:h-[500px] lg:w-[620px] lg:h-[465px] xl:w-[773px] xl:h-[580px] xl:shrink-0 aspect-[773/580] xl:aspect-auto px-3 sm:px-0 lg:pr-15 xl:pr-22">
             <ImageFrame
               placement="hero"
               src="/images/hero/hero-image.webp"
               alt="Hero image"
-              className="h-full w-full object-cover sm:relative sm:bottom-5 md:bottom-10"
+              className="h-full w-full object-cover sm:relative sm:bottom-5 lg:bottom-6 xl:bottom-8"
             />
           </div>
         </div>
