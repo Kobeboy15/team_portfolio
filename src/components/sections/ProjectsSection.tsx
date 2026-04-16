68cbcb16e9f11d9aa53baad22483b7bad71beeb8
EnriqueSaracho <esarachof97@gmail.com>
2026-04-16 14:47:05 -0700

    fix: update separation between projects

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -10,9 +10,17 @@ export function ProjectsSection() {
     <Section id="projects" spacing="sm" className="w-full bg-background" withSectionGap>
       <div className="mx-5 flex flex-col gap-10 md:mx-7 lg:mx-10 xl:mx-20 lg:gap-16">
         <div className="flex flex-col gap-16 lg:gap-24">
-          <div className="flex flex-col gap-14 md:gap-16 lg:hidden">
-            {projects.map((project) => (
-              <ProjectCard key={project.id} project={project} />
+          <div className="flex flex-col gap-16 md:gap-20 lg:hidden">
+            {projects.map((project, index) => (
+              <div key={project.id} className="flex flex-col">
+                <ProjectCard project={project} />
+                {index < projects.length - 1 ? (
+                  <div
+                    className="mx-auto mt-10 h-px w-[94%] max-w-[46rem] bg-border"
+                    aria-hidden="true"
+                  />
+                ) : null}
+              </div>
             ))}
           </div>
 
