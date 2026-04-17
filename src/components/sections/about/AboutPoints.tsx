e20b78a9f58727ffadac40d437c2405bcaa0fc2e
EnriqueSaracho <esarachof97@gmail.com>
2026-04-17 14:12:28 -0700

    feat: adjust about points spacing on mobile

--- src/components/sections/about/AboutPoints.tsx
+++ src/components/sections/about/AboutPoints.tsx
@@ -7,7 +7,7 @@ export function AboutPoints() {
       <div className="flex w-full flex-col items-start justify-center gap-8 sm:flex-row sm:gap-0 lg:h-[calc(100dvh-72px)] lg:w-[min(100vw,2500px)] lg:flex-col">
         {aboutData.points.map((point, index) => (
           <div key={index} className="w-full">
-            <div className="mx-5 flex max-w-full flex-col px-7 py-2 font-sora sm:max-w-[100vw] sm:px-12 sm:py-6 lg:mx-0 lg:flex-row lg:gap-10 lg:px-[10vw] 2xl:text-sora-18">
+            <div className="mx-5 flex max-w-full flex-col px-7 py-10 font-sora sm:max-w-[100vw] sm:px-12 sm:py-6 lg:mx-0 lg:flex-row lg:gap-10 lg:px-[10vw] 2xl:text-sora-18">
               <ScrollReveal>
                 <h3 className="min-w-48 lg:mb-5">{point.title}</h3>
               </ScrollReveal>
