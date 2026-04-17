903212a30d5ffe669589c1f6fcecf8816312ebbe
EnriqueSaracho <esarachof97@gmail.com>
2026-04-17 14:03:43 -0700

    fix: adjust projects separator to be symmetric on mobile - address coderabbitai comment

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -10,13 +10,13 @@ export function ProjectsSection() {
     <Section id="projects" spacing="sm" className="w-full bg-background" withSectionGap>
       <div className="mx-5 flex flex-col gap-10 md:mx-7 lg:mx-10 xl:mx-20 lg:gap-16">
         <div className="flex flex-col gap-16 lg:gap-24">
-          <div className="flex flex-col gap-16 md:gap-20 lg:hidden">
+          <div className="flex flex-col lg:hidden">
             {projects.map((project, index) => (
               <div key={project.id} className="flex flex-col">
                 <ProjectCard project={project} />
                 {index < projects.length - 1 ? (
                   <div
-                    className="mx-auto mt-10 h-px w-[94%] max-w-[46rem] bg-border"
+                    className="mx-auto my-8 h-px w-[94%] max-w-[46rem] bg-border md:my-10"
                     aria-hidden="true"
                   />
                 ) : null}
